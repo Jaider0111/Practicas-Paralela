368ca453f083892bb2dd71b4d0d142740f5375ea
Jaider Andres Pinto Pinto <jaider.pinto@hotmail.com>
2022-04-23 23:23:13 -0500

    finalizando

--- Practica1/run_examples.sh
+++ Practica1/run_examples.sh
@@ -7,16 +7,16 @@ fi
 
 for i in {1..12}
 do
-	echo ./image-filter perrito720.jpg "'720 filtred/perrito720F$i.jpg'" $i $1
-   	time ./image-filter perrito720.jpg "720 filtred/perrito720F$i.jpg" $i $1
+	echo ./image-filter perrito720.jpg "'720 filtrado/perrito720F$i.jpg'" $i $1
+   	time ./image-filter perrito720.jpg "720 filtrado/perrito720F$i.jpg" $i $1
 done
 for i in {1..12}
 do
-	echo ./image-filter perrito1080.jpg "'1080 filtred/perrito1080F$i.jpg'" $i $1
-   	time ./image-filter perrito1080.jpg "1080 filtred/perrito1080F$i.jpg" $i $1
+	echo ./image-filter perrito1080.jpg "'1080 filtrado/perrito1080F$i.jpg'" $i $1
+   	time ./image-filter perrito1080.jpg "1080 filtrado/perrito1080F$i.jpg" $i $1
 done
 for i in {1..12}
 do
-	echo ./image-filter perrito4K.jpg "'4k filtred/perrito4KF$i.jpg'" $i $1
-   	time ./image-filter perrito4K.jpg "4k filtred/perrito4KF$i.jpg" $i $1
+	echo ./image-filter perrito4K.jpg "'4k filtrado/perrito4KF$i.jpg'" $i $1
+   	time ./image-filter perrito4K.jpg "4k filtrado/perrito4KF$i.jpg" $i $1
 done
