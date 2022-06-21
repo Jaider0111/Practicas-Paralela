1d1e965b5296f0c6bc69c4c89ef5fb2da1391c0b
Jaider Andres Pinto Pinto <jaider.pinto@hotmail.com>
2022-06-20 23:47:52 -0500

    Semi avace

---
+++ Parcial/input-gen.py
@@ -0,0 +1,9 @@
+from random import randint as ri
+
+n = int(input())
+with open('input.txt', 'w') as file:
+    file.write(f'{n}\n')
+    for i in range(2*n):
+        for j in range(n):
+            file.write(f'{ri(0,1000)} ')
+        file.write(f'\n')
